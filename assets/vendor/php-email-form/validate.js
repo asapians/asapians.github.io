cc1a707063bff16895bf781b12b20c609e012a84
OV <arun.ov@uniathena.com>
2025-10-01 17:44:45 +0530

    Mail Chimp Submit

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,23 +1,105 @@
 /**
-* PHP Email Form Validation - v3.11
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
+ * PHP Email Form Validation - v3.11
+ * URL: https://bootstrapmade.com/php-email-form/
+ * Author: BootstrapMade.com
+ */
 (function () {
   "use strict";
 
+  // let forms = document.querySelectorAll('.php-email-form');
+
+  // forms.forEach( function(e) {
+  //   e.addEventListener('submit', function(event) {
+  //     event.preventDefault();
+
+  //     let thisForm = this;
+
+  //     let action = thisForm.getAttribute('action');
+  //     let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+
+  //     if( ! action ) {
+  //       displayError(thisForm, 'The form action property is not set!');
+  //       return;
+  //     }
+  //     thisForm.querySelector('.loading').classList.add('d-block');
+  //     thisForm.querySelector('.error-message').classList.remove('d-block');
+  //     thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+  //     let formData = new FormData( thisForm );
+
+  //     if ( recaptcha ) {
+  //       if(typeof grecaptcha !== "undefined" ) {
+  //         grecaptcha.ready(function() {
+  //           try {
+  //             grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+  //             .then(token => {
+  //               formData.set('recaptcha-response', token);
+  //               php_email_form_submit(thisForm, action, formData);
+  //             })
+  //           } catch(error) {
+  //             displayError(thisForm, error);
+  //           }
+  //         });
+  //       } else {
+  //         displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+  //       }
+  //     } else {
+  //       php_email_form_submit(thisForm, action, formData);
+  //     }
+  //   });
+  // });
+
+  // function php_email_form_submit(thisForm, action, formData) {
+  //   fetch(action, {
+  //     method: 'POST',
+  //     body: formData
+  //     // headers: {'X-Requested-With': 'XMLHttpRequest'}
+  //   })
+  //   .then(response => {
+  //     if( response.ok ) {
+  //       return response.text();
+  //     } else {
+  //       throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+  //     }
+  //   })
+  //   .then(data => {
+  //     thisForm.querySelector('.loading').classList.remove('d-block');
+  //     if (data.trim() == 'OK') {
+  //       thisForm.querySelector('.sent-message').classList.add('d-block');
+  //       thisForm.reset();
+  //     } else {
+  //       throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action);
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     displayError(thisForm, error);
+  //   });
+  // }
+
+  function displayError(thisForm, error) {
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+
+  function formSendSuccess(thisForm) {
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.sent-message').classList.add('d-block');
+    thisForm.reset();
+  }
+
   let forms = document.querySelectorAll('.php-email-form');
 
-  forms.forEach( function(e) {
-    e.addEventListener('submit', function(event) {
+  forms.forEach(function (e) {
+    e.addEventListener('submit', function (event) {
       event.preventDefault();
 
       let thisForm = this;
 
       let action = thisForm.getAttribute('action');
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
-      if( ! action ) {
+
+      if (!action) {
         displayError(thisForm, 'The form action property is not set!');
         return;
       }
@@ -25,18 +107,20 @@
       thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
 
-      let formData = new FormData( thisForm );
+      let formData = new FormData(thisForm);
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
+      if (recaptcha) {
+        if (typeof grecaptcha !== "undefined") {
+          grecaptcha.ready(function () {
             try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+              grecaptcha.execute(recaptcha, {
+                action: 'php_email_form_submit'
+              })
               .then(token => {
                 formData.set('recaptcha-response', token);
                 php_email_form_submit(thisForm, action, formData);
               })
-            } catch(error) {
+            } catch (error) {
               displayError(thisForm, error);
             }
           });
@@ -50,36 +134,26 @@
   });
 
   function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
-      method: 'POST',
-      body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
-    })
-    .then(response => {
-      if( response.ok ) {
-        return response.text();
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+    $(thisForm).ajaxChimp({
+      url: action,
+      callback: function (response) {
+        if (response.result === 'success') {
+          formSendSuccess(thisForm);
+        } else {
+          displayError(thisForm, response.msg);
+        }
       }
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
     });
-  }
 
-  function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
+    // $.ajaxChimp.translations.es = {
+    //   'submit': 'Submitting...',
+    //   0: 'We have sent you a confirmation email',
+    //   1: 'Please enter a valid e-mail address',
+    //   2: 'E-mail address is not valid',
+    //   3: 'E-mail address is not valid',
+    //   4: 'E-mail address is not valid',
+    //   5: 'E-mail address is not valid'
+    // };
   }
 
 })();
