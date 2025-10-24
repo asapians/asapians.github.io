88659d1141453331c34bacaef2c4e3f89ca8f4e7
asapians <asapiansindia@gmail.com>
2025-10-25 00:16:11 +0530

    Mascot Updated & Contact form speed up

--- assets/js/mail-chimp-form.js
+++ assets/js/mail-chimp-form.js
@@ -19,17 +19,12 @@
     forms.forEach(function (e) {
         e.addEventListener('submit', function (event) {
             event.preventDefault();
-
             let thisForm = this;
             let action = thisForm.getAttribute('action');
             let recaptcha = thisForm.getAttribute('data-sitekey');
 
-            console.log(thisForm);
-            console.log(action);
-            console.log(recaptcha);
-
             if (!action) {
-                displayError(thisForm, 'The form action property is not set!');
+                displayError(thisForm, 'Looks like something is wrong contact our team!');
                 return;
             }
             thisForm.querySelector('.loading').classList.add('d-block');
@@ -37,31 +32,60 @@
             thisForm.querySelector('.sent-message').classList.remove('d-block');
 
             let formData = new FormData(thisForm);
+            fetchMailchimpSubmit(thisForm, formData, action);
 
-            if (recaptcha) {
-                if (typeof grecaptcha !== "undefined") {
-                    grecaptcha.ready(function () {
-                        try {
-                            grecaptcha.execute(recaptcha, {
-                                action: 'php_email_form_submit'
-                            })
-                            .then(token => {
-                                formData.set('recaptcha-response', token);
-                                php_email_form_submit(thisForm, action, formData);
-                                ajaxMailchimpSubmit(thisForm, action, formData);
-                            })
-                        } catch (error) {
-                            displayError(thisForm, error);
-                        }
-                    });
-                } else {
-                    displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-                }
-            } else {
-                ajaxMailchimpSubmit(thisForm, action, formData);
-            }
+            //Captch verification
+
+            // if (recaptcha) {
+            //     if (typeof grecaptcha !== "undefined") {
+            //         grecaptcha.ready(function () {
+            //             try {
+            //                 grecaptcha.execute(recaptcha, {
+            //                     action: 'php_email_form_submit'
+            //                 })
+            //                 .then(token => {
+            //                     formData.set('recaptcha-response', token);
+            //                     php_email_form_submit(thisForm, action, formData);
+            //                     ajaxMailchimpSubmit(thisForm, action, formData);
+            //                 })
+            //             } catch (error) {
+            //                 displayError(thisForm, error);
+            //             }
+            //         });
+            //     } else {
+            //         displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+            //     }
+            // } else {
+            //     ajaxMailchimpSubmit(thisForm, action, formData);
+            // }
         });
     });
+
+    function fetchMailchimpSubmit(thisForm, formData, action) {
+        // Convert FormData to URL-encoded string (Mailchimp expects this)
+        let params = new URLSearchParams();
+        formData.forEach((value, key) => {
+            params.append(key, value);
+        });
+
+        // Send via fetch
+        fetch(action, {
+            method: 'POST',
+            mode: 'no-cors', // Mailchimp doesn’t allow CORS
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: params.toString()
+        })
+        .then(() => {
+            // Mailchimp doesn’t return JSON with no-cors, so assume success
+            formSendSuccess(thisForm);
+        })
+        .catch((error) => {
+            displayError(thisForm, 'Submission failed! Please try again.');
+            console.error(error);
+        });
+    }
 
     function ajaxMailchimpSubmit(thisForm, action, formData) {
         $(thisForm).ajaxChimp({
